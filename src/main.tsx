613cfbdd33228b17f10088fc8c3f3edc3d3327cd
unknown <milanpanchal169@gmail.com>
2025-04-15 22:35:09 +0530

    responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import "./index.css";
+import App from "./App.tsx";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react"
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider value={defaultSystem}>
+        <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
